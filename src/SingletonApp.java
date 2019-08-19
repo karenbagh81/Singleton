32de38bfba576f82a7c12459153bc3734bffa4fb
Karen <kbaghdasaryan81@gmail.com>
2019-08-19 17:29:11 +0400

    Change SIZE;

--- src/SingletonApp.java
+++ src/SingletonApp.java
@@ -1,7 +1,7 @@
 public class SingletonApp {
     public static void main(String[] args) {
 
-        final int SIZE = 10;
+        final int SIZE = 20;
 
         Thread thread[] = new Thread[SIZE];
 
