ee01ed1203b2183b9fb18b8697b3064482b2649d
Karen <kbaghdasaryan81@gmail.com>
2019-08-19 16:52:05 +0400

    First commit

---
+++ src/SingletonApp.java
@@ -0,0 +1,51 @@
+public class SingletonApp {
+    public static void main(String[] args) {
+
+        final int SIZE = 100;
+
+        Thread thread[] = new Thread[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            thread[i] = new Thread(new Thr());
+            thread[i].start();
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            try {
+                thread[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(Singleton.count);
+    }
+}
+
+class Thr implements Runnable {
+
+    @Override
+    public void run() {
+        Singleton.getInstance();
+    }
+}
+
+class Singleton {
+    public static int count = 0;
+
+    private static volatile Singleton instance = null;
+
+    private Singleton() {
+        count++;
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null)
+                    instance = new Singleton();
+            }
+        }
+        return instance;
+    }
+}
